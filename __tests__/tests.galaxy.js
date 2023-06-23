b1c6ed35362fa9c354dbdd3dee16aa6c60a79724
Natalie Benjes <natalie.benjes@gmail.com>
2023-06-23 08:49:00 -0700

    set up folder

---
+++ __tests__/tests.galaxy.js
@@ -0,0 +1,15 @@
+import Person from './../src/js/galaxy.js';
+
+describe("Person",  () => {
+
+  let person;
+
+  beforeEach(() => {
+      person = new Person("Shauna", 35);
+  });
+
+  test("Make a person object", () => {
+      expect(person.name).toEqual("Shauna");
+      expect(person.age).toEqual(35);
+  });
+})
